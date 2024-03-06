11f4bfffb048cd8e75909a04e501b2a465b6bf54
edd-ie <ogendoeddie@gmail.com>
2024-03-05 21:41:36 -0400

    FloodFill and manhattan distance algorithms

--- Maze.java
+++ Maze.java
@@ -16,6 +16,7 @@ public class Maze {
 
         MazeSolver maze1 = new MazeSolver(map1);
         System.out.println("\nMap 1:\n"+maze1.draw());
-        System.out.println("\nDistance 1:\n"+maze1.draw(maze1.distanceMap()));
+        System.out.println("\nDistance from entrance:\n"+maze1.draw(maze1.distanceMap()));
+        System.out.println("\nFlood Fill:\n"+maze1.draw(maze1.floodFill(0,3, map1.length-1, 7)));
     }
 }
--- MazeSolver.java
+++ MazeSolver.java
@@ -1,6 +1,7 @@
 public class MazeSolver {
     private final int[][] maze;
 
+
     public MazeSolver(int[][] maze){
         this.maze = maze;
     }
@@ -8,12 +9,12 @@ public class MazeSolver {
     public String draw(){
         String draw = "";
 
-        for(int i = 0; i < maze.length;i++){
-            for(int x = 0; x < maze.length;x++){
-                if(maze[i][x] == 0) draw+="[*]";
-                else draw+="   ";
+        for (int[] row : maze) {
+            for (int x = 0; x < maze.length; x++) {
+                if (row[x] == 0) draw += "[*]";
+                else draw += "   ";
             }
-            draw+="\n";
+            draw += "\n";
         }
 
         return draw;
@@ -21,15 +22,15 @@ public class MazeSolver {
     public String draw(int[][] map){
         String draw = "";
 
-        for(int i = 0; i < map.length;i++){
-            for(int x = 0; x < map.length;x++){
-                if(map[i][x] == 0) draw+="[*]";
-                else{
-                    if(map[i][x] > 9) draw += map[i][x]+" ";
-                    else draw += " " + map[i][x] + " ";
+        for (int[] row : map) {
+            for (int x = 0; x < map.length; x++) {
+                if (row[x] == 0) draw += "[*]";
+                else {
+                    if (row[x] > 9) draw += row[x] + " ";
+                    else draw += " " + row[x] + " ";
                 }
             }
-            draw+="\n";
+            draw += "\n";
         }
 
         return draw;
@@ -47,11 +48,48 @@ public class MazeSolver {
                         home[1] = y;
                     }
                     int value = Math.abs(home[0]-x)+Math.abs(home[1]-y);
-                    map[x][y] = value == 0 ? -1 : value; // Manhattan distance
+                    map[x][y] = value == 0 ? 1 : value; // Manhattan distance
                 }
             }
         }
 
         return map;
+    }
+
+    public int[][] floodFill(int x, int y, int endX, int endY){
+        if(x == endX && y == endY) return maze;
+        int min = this.maze[x][y];
+
+        if(x > 0 && maze[x-1][y] != 0)
+            min = Math.min(min, this.maze[x-1][y]);
+
+        if(x < this.maze.length-1 && maze[x+1][y] != 0)
+            min = Math.min(min, this.maze[x+1][y]);
+
+        if(y > 0 && maze[x][y-1] != 0)
+            min = Math.min(min, this.maze[x][y-1]);
+
+        if(y < this.maze[x].length-1 && maze[x][y+1] != 0)
+            min = Math.min(min, this.maze[x][y+1]);
+
+
+        if(this.maze[x][y] <= min){
+            this.maze[x][y] = min+1;
+
+            if(x > 0 && maze[x-1][y] != 0)
+                floodFill(x-1, y, endX, endY);
+
+            if(x < this.maze.length-1 && maze[x+1][y] != 0)
+                floodFill(x+1, y, endX, endY);
+
+            if(y > 0 && maze[x][y-1] != 0)
+                floodFill(x, y-1, endX, endY);
+
+            if(y < this.maze[x].length-1 && maze[x][y+1] != 0)
+                floodFill(x, y+1, endX, endY);
+        }
+
+
+        return maze;
     }
 }
